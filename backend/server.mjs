553c99d30a7088619af21bddf44a0cfe4d599b07
SnirNahum <snir09@gmail.com>
2023-09-17 22:24:23 +0300

    Revert "add build"
    This reverts commit 701e82e13f3ff8d9699c1eb1fafd138bb657a87c.

--- backend/server.mjs
+++ backend/server.mjs
@@ -20,7 +20,6 @@ if (process.env.NODE_ENV === "production") {
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://dreamteam-yidh.onrender.com/",
     ],
     credentials: true,
   };
