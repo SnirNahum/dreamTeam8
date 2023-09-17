49318a6f8ed48a7cedb0bd12f9aade54af1486a9
SnirNahum <snir09@gmail.com>
2023-09-17 23:07:29 +0300

    organize code for deploy

--- backend/server.mjs
+++ backend/server.mjs
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === "production") {
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
+      "https://dreamteam-yidh.onrender.com/",
     ],
     credentials: true,
   };
