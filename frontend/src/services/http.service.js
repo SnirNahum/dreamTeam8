4b77e5f37ec2b8b5c38e6201ab3369acb7fb7314
snir nahum <snir09@gmail.com>
2024-03-24 17:46:24 +0200

    b

--- frontend/src/services/http.service.js
+++ frontend/src/services/http.service.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 // import { router } from '@/router'
 
 const BASE_URL =
-  process.env.NODE_ENV === "//production" ? "//localhost:10000/api/" : "//localhost:10000/api/";
+  process.env.NODE_ENV === "production" ? "/api/" : "//localhost:10000/api/";
 
 const axios = Axios.create({
   withCredentials: true,
