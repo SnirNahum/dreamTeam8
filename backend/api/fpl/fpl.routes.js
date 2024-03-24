0c9127bd88db637af36a926e7c785da05b61b42a
snir nahum <snir09@gmail.com>
2024-03-24 17:50:49 +0200

    vd

--- backend/api/fpl/fpl.routes.js
+++ backend/api/fpl/fpl.routes.js
@@ -4,7 +4,7 @@ import { getGeneralInfo, getPlayerInfo } from "./fpl.controller.js";
 
 const router = express.Router();
 
-router.get("/generalInfo", getGeneralInfo);
-router.get("/playerInfo", getPlayerInfo);
+router.get("generalInfo", getGeneralInfo);
+router.get("playerInfo", getPlayerInfo);
 
 export const fplRoutes = router;
