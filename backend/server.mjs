41b121163fb08405adcc465cdba776a4d83fead9
snir nahum <snir09@gmail.com>
2024-04-03 14:47:50 +0300

    0.0.0.0

--- backend/server.mjs
+++ backend/server.mjs
@@ -4,17 +4,18 @@ import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
 import { logger } from "./services/logger.service.js";
+import { fplRoutes } from "./api/fpl/fpl.routes.js";
+
 const app = express();
 const server = http.createServer(app);
 
-// Express App Config
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 
-
+// CORS Configuration
 if (process.env.NODE_ENV === "production") {
-  logger.error(`process.env.NODE_ENV --- ${process.env.NODE_ENV}`);
-  app.use(express.static(("public")));
+  app.use(express.static("public"));
 } else {
   const corsOptions = {
     origin: [
@@ -25,24 +26,31 @@ if (process.env.NODE_ENV === "production") {
       "http://3.125.183.140",
       "http://3.75.158.163",
       "http://35.157.117.28",
-      "https://dreamteam8.onrender.com/",
+      "http://0.0.0.0",
+      "https://dreamteam8-1.onrender.com/",
     ],
     credentials: true,
   };
   app.use(cors(corsOptions));
 }
-logger.error(`process.env.NODE_ENV --- ${process.env.NODE_ENV}`);
-import { fplRoutes } from "./api/fpl/fpl.routes.js";
 
+// Routes
 app.use("/api", fplRoutes);
 
+// Catch-all route to serve index.html for client-side routing
 app.get("/**", (req, res) => {
   res.sendFile(path.resolve("public/index.html"));
 });
 
-const port = 10000;
+// Error handling middleware
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Server configuration
+const port = process.env.PORT || 3030; // Use environment variable if available
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   logger.info(`Server is running on port ${port}`);
-
 });
