7aca6e34f15f87d586f90db8dd26d71872fbdc72
snir nahum <snir09@gmail.com>
2024-04-03 23:37:24 +0300

    added proxy

--- backend/api/fpl/fpl.service.js
+++ backend/api/fpl/fpl.service.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { logger } from "../../services/logger.service.js";
-import {HttpsProxyAgent} from "https-proxy-agent";
+import { HttpsProxyAgent } from "https-proxy-agent";
 
 async function GeneralInfo() {
   try {
-    console.log("work");
     const BASE_URL = "https://fantasy.premierleague.com/api/";
-    const TIMEOUT_MS = 15000;
     const agent = new HttpsProxyAgent(`http://sgvygpyc-rotate:bnq2scmvmpmv@p.webshare.io:80/`);
     const response = await axios.get(`${BASE_URL}bootstrap-static/`, {
       httpsAgent: agent,
@@ -14,7 +12,6 @@ async function GeneralInfo() {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Safari/605.1.15',
       },
 
-      timeout: TIMEOUT_MS,
     });
     console.log("response: ", response);
     return response.data;
@@ -26,10 +23,13 @@ async function GeneralInfo() {
 async function PlayerInfo(playerId) {
   try {
     const BASE_URL = "https://fantasy.premierleague.com/api/";
-    const TIMEOUT_MS = 15000;
+    const agent = new HttpsProxyAgent(`http://sgvygpyc-rotate:bnq2scmvmpmv@p.webshare.io:80/`);
 
     const response = await axios.get(`${BASE_URL}element-summary/${playerId}`, {
-      timeout: TIMEOUT_MS,
+      httpsAgent: agent,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Safari/605.1.15',
+      },
     });
     return response.data;
   } catch (err) {
