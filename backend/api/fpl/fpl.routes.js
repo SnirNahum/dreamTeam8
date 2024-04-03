e863380199801275d1460593b6255ce3ca3f983a
snir nahum <snir09@gmail.com>
2024-04-03 23:06:40 +0300

    a

--- backend/api/fpl/fpl.routes.js
+++ backend/api/fpl/fpl.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { getGeneralInfo, getPlayerInfo, gettesting } from "./fpl.controller.js";
+import { getGeneralInfo, getPlayerInfo } from "./fpl.controller.js";
 
 const router = express.Router();
 
 router.get("/generalInfo", getGeneralInfo);
 router.get("/playerInfo", getPlayerInfo);
-router.get("/test", gettesting);
 
 export const fplRoutes = router;
