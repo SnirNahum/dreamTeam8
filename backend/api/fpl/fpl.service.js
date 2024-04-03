d19c770d89fa59b1c966f91bc1d67fe3484afe13
ahmar7 <ahmarjabbar7@gmail.com>
2024-04-04 00:14:23 +0500

    minor api call handling fixed

--- backend/api/fpl/fpl.service.js
+++ backend/api/fpl/fpl.service.js
@@ -5,7 +5,11 @@ async function GeneralInfo() {
   try {
     console.log("work");
     const BASE_URL = "https://fantasy.premierleague.com/api/";
-    const response = await axios.get(`${BASE_URL}bootstrap-static`);
+    const TIMEOUT_MS = 15000;
+
+    const response = await axios.get(`${BASE_URL}bootstrap-static`, {
+      timeout: TIMEOUT_MS,
+    });
     console.log("response: ", response);
     return response.data;
   } catch (err) {
@@ -16,7 +20,11 @@ async function GeneralInfo() {
 async function PlayerInfo(playerId) {
   try {
     const BASE_URL = "https://fantasy.premierleague.com/api/";
-    const response = await axios.get(`${BASE_URL}element-summary/${playerId}`);
+    const TIMEOUT_MS = 15000;
+
+    const response = await axios.get(`${BASE_URL}element-summary/${playerId}`, {
+      timeout: TIMEOUT_MS,
+    });
     return response.data;
   } catch (err) {
     logger.error("cannot find PlayerInfo", err);
